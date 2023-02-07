158a10c7ec9f803d47ba28ab9223ff88b9a1b46a
ols45234 <96008059+ols45234@users.noreply.github.com>
2023-02-07 18:56:41 +0300

    Add files via upload
    changed rotations to set cource

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-/*var canvas = document.getElementById("canvas");
+var canvas = document.getElementById("canvas");
 var canvaswidth = 512;
 var canvasheight = 512;
 
 canvas.width = canvaswidth;
 canvas.height = canvasheight;
-var ctx = canvas.getContext("2d");*/
+var ctx = canvas.getContext("2d");
 var dia = Math.sqrt(2)
 
 function compile() {
     let input = document.getElementById("i").value
+	//input = input.replaceAll("pass", document.getElementById("p").value)
     let output = ''
-    let turtle = {x: 0, y: 0, stepSize: 1}
-	input.split('\n').forEach((Finst, id) => {
+    let turtle = {x: 0, y: 0, stepSize: 1, loop: 0, loopStart: 0, inLoop: false, cx: 0, cy: 0}
+	let inputArr = input.split('\n')
+	for(var id = 0; id < inputArr.length; id++) {
+		let inst = inputArr[id]
+		if(inst.split(' ')[0] == 'call')
+			input = input.replaceAll(inst, (document.getElementById(`func_${inst.split(' ')[1]}`).value))
+	}
+	inputArr = input.split('\n')
+
+	
+	for(var id = 0; id < inputArr.length; id++) {
+		let Finst = inputArr[id]
+	
 		let inst = Finst.split(' ')[0];
 		let args = Finst.split(' ').map(i => parseInt(i));
 		args.shift();
@@ -20,53 +32,53 @@ function compile() {
 		arg = Math.abs(arg)
 		arg = arg * turtle.stepSize;
 		if(inst == "u") {
-			output += `пр 0 ${mc} ${arg} лв 0 `
+			output += (turtle.inLoop) ? '' : `нк 0 ${mc} ${arg} `
 			turtle.y += arg;
 		}
 		else if(inst == "d") {
-			output += `пр 180 ${mc} ${arg} лв 180 `
+			output += (turtle.inLoop) ? '' : `нк 180 ${mc} ${arg} `
 			turtle.y -= arg;
 		}
 		else if(inst == "l") {
 			turtle.x -= arg;
-			output += `пр 270 ${mc} ${arg} лв 270 `
+			output += (turtle.inLoop) ? '' : `нк 270 ${mc} ${arg} `
 		}
 		else if(inst == "r") {
-			output += `пр 90 ${mc} ${arg} лв 90 `
+			output += (turtle.inLoop) ? '' : `нк 90 ${mc} ${arg} `
 			turtle.x += arg;
 		}
 		else if(inst == "ul") {
-			output += `пр 315 ${mc} ${dia * arg} лв 315 `
+			output += (turtle.inLoop) ? '' : `нк 315 ${mc} ${dia * arg} `
 			turtle.x -= arg;
 			turtle.y += arg;
 		}
 		else if(inst == "ur") {
-			output += `пр 45 ${mc} ${dia * arg} лв 45 `
+			output += (turtle.inLoop) ? '' : `нк 45 ${mc} ${dia * arg} `
 			turtle.x += arg;
 			turtle.y += arg;
 		}
 		else if(inst == "dl") {
-			output += `пр 225 ${mc} ${dia * arg} лв 225 `
+			output += (turtle.inLoop) ? '' : `нк 225 ${mc} ${dia * arg} `
 			turtle.x -= arg;
 			turtle.y -= arg;
 		}
 		else if(inst == "dr") {
-			output += `пр 135 ${mc} ${dia * arg} лв 135 `
+			output += (turtle.inLoop) ? '' : `нк 135 ${mc} ${dia * arg} `
 			turtle.x += arg;
 			turtle.y -= arg;
 		}
 		else if(["go", "goto", "g"].includes(inst)) {
 			let pNow = turtle
 			let endPos = {x: args[0], y: args[1]}
-			let x = endPos.x - pNow.x;
-			let y = endPos.y - pNow.y;
+			let x = endPos.x - pNow.x + pNow.cx;
+			let y = endPos.y - pNow.y + pNow.cy;
 			let oper = 1
 			
 			if(x < 0)
 				oper = -1
 			let calc = (Math.acos(y / Math.sqrt(y * y + x * x)) / Math.PI * 180).toFixed(5); // calculate rotation and convert to degrees
 			let dist = Math.sqrt(y * y + x * x);
-			output += `${oper > 0 ? "пр" : "лв"} ${calc} вп ${dist} ${oper < 0 ? "пр" : "лв"} ${calc} `.replaceAll('.', ',')
+			output += (turtle.inLoop) ? '' : `${oper > 0 ? "пр" : "лв"} ${calc} вп ${dist} ${oper < 0 ? "пр" : "лв"} ${calc} `.replaceAll('.', ',')
 			turtle.x = args[0]
 			turtle.y = args[1]
 		}
@@ -83,7 +95,7 @@ function compile() {
 				oper = -1
 			let calc = (Math.acos(y / Math.sqrt(y * y + x * x)) / Math.PI * 180).toFixed(5); // calculate rotation and convert to degrees
 			let dist = Math.sqrt(y * y + x * x);
-			output += `${oper > 0 ? "пр" : "лв"} ${calc} вп ${dist} ${oper < 0 ? "пр" : "лв"} ${calc} `.replaceAll('.', ',')
+			output += (turtle.inLoop) ? '' : `${oper > 0 ? "пр" : "лв"} ${calc} вп ${dist} ${oper < 0 ? "пр" : "лв"} ${calc} `.replaceAll('.', ',')
 			turtle.x = turtle.x + args[0]
 			turtle.y = turtle.y + args[1]
 			
@@ -93,12 +105,37 @@ function compile() {
 		else if(inst == "pd")
 			output += "по "
 		else if(inst == "*") {
-			turtle.stepSize = arg;
+			turtle.stepSize = args[0];
+		}
+		else if(["re", "repeat", "rep"].includes(inst)) {
+			output += `повтори ${args[0]} [`
+			turtle.loopStart = id;
+			turtle.loop = args[0];
+			
+		}
+		else if(["ere", "endrepeat", "endrep", "er"].includes(inst)) {
+			turtle.loop -= 1
+			turtle.inLoop = true
+			if(turtle.loop == 0) {
+				output += `] `
+				turtle.inLoop = false 
+			}
+			else
+				id = turtle.loopStart
+			
+		}
+		else if(["sc", "sz"].includes(inst)) {
+			turtle.cx = turtle.x
+			turtle.cy = turtle.y
+			
+		}
+		else {
+			output += Finst
 		}
 		//debugger
 		console.log(turtle)
 		
-	})
+	}
 	
     document.getElementById("o").value = output
 }
@@ -106,3 +143,63 @@ function compile() {
 function toCircularInt(num, limit) {
 	return Math.ceil((-num) / limit) * limit + num
 }
+
+function redraw() {
+	ctx.clearRect(0, 0, canvaswidth, canvasheight)
+	ctx.beginPath()
+	ctx.fillStyle = 'black'
+	ctx.strokeStyle = 'black'
+	ctx.arc(256, 256, 10, 0, Math.PI * 2)
+	ctx.fill()
+	ctx.stroke()
+	ctx.closePath()
+
+	for(let i = 0; i < 8; i ++) {
+		let angle = +document.getElementById('a').value
+		angle = isNaN(angle) ? 30 : angle  
+		let ang = i % 2 == 0 ? Math.floor(i / 2) * 90 - angle : Math.floor(i / 2) * 90 + angle
+		//console.log(ang, angle)
+		ang = ang / 180 * Math.PI
+		ctx.beginPath()
+		ctx.fillStyle = 'black'
+		ctx.strokeStyle = 'black'
+		ctx.moveTo(256, 256)
+		ctx.lineTo(256 + Math.cos(ang) * 128, 256 + Math.sin(ang) * 128)
+		ctx.stroke()
+		ctx.closePath()
+	}
+}
+
+function reformPasses() {
+	let input = document.getElementById("i").value
+	let inputArr = input.split('\n')
+	let passes = []
+	for(var id = 0; id < inputArr.length; id++) {
+		let inst = inputArr[id].split(' ')
+		if(inst[0] == 'call' && !passes.includes(`func_${inst[1]}`))
+			passes.push(`func_${inst[1]}`)
+	}
+	//console.log(passes)
+	
+	passElems = document.getElementById('passes').children
+	
+	for(let i = 0; i < passElems.length; i++) {
+		elem = passElems[i]
+		if(!(passes.includes(elem.id))) // удаляем текстовое поле если такого на добавление нету
+			elem.remove()
+		else
+			passes[passes.findIndex(function (e) {return e == elem.id})] = undefined //удаляем из списка на создание если такой есть
+	}
+	passes.forEach(i => { //добовляем
+		if(!i)
+			return
+		elem = document.createElement('textarea');
+		elem.id = i;
+		elem.style = "width:200px;height:200px";
+		elem.placeholder = `print code of function "${i.slice(5)}" here...`;
+		document.getElementById('passes').appendChild(elem)
+	})
+	
+}
+
+setInterval(() => redraw(), 500)
