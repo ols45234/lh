b425910ff9fcf93210174a29cd8f0af9d4cf4a91
ols45234 <96008059+ols45234@users.noreply.github.com>
2023-02-09 20:47:10 +0300

    Add files via upload
    added pattern "uzor"

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function compile() {
     let input = document.getElementById("i").value
 	//input = input.replaceAll("pass", document.getElementById("p").value)
     let output = ''
-    let turtle = {x: 0, y: 0, stepSize: 1, loop: 0, loopStart: 0, inLoop: false, cx: 0, cy: 0}
+    let turtle = {x: 0, y: 0, stepSize: 1, loop: 0, loopStart: 0, inLoop: false, cx: 0, cy: 0, px: 0, py: 0}
 	let inputArr = input.split('\n')
 	for(var id = 0; id < inputArr.length; id++) {
 		let inst = inputArr[id]
@@ -32,72 +32,73 @@ function compile() {
 		arg = Math.abs(arg)
 		arg = arg * turtle.stepSize;
 		if(inst == "u") {
-			output += (turtle.inLoop) ? '' : `пр 0 ${mc} ${arg} лв 0 `
+			output += (turtle.inLoop) ? '' : `нк 0 ${mc} ${arg} `
 			turtle.y += arg;
 		}
 		else if(inst == "d") {
-			output += (turtle.inLoop) ? '' : `пр 180 ${mc} ${arg} лв 180 `
+			output += (turtle.inLoop) ? '' : `нк 180 ${mc} ${arg} `
 			turtle.y -= arg;
 		}
 		else if(inst == "l") {
 			turtle.x -= arg;
-			output += (turtle.inLoop) ? '' : `пр 270 ${mc} ${arg} лв 270 `
+			output += (turtle.inLoop) ? '' : `нк 270 ${mc} ${arg} `
 		}
 		else if(inst == "r") {
-			output += (turtle.inLoop) ? '' : `пр 90 ${mc} ${arg} лв 90 `
+			output += (turtle.inLoop) ? '' : `нк 90 ${mc} ${arg} `
 			turtle.x += arg;
 		}
 		else if(inst == "ul") {
-			output += (turtle.inLoop) ? '' : `пр 315 ${mc} ${dia * arg} лв 315 `
+			output += (turtle.inLoop) ? '' : `нк 315 ${mc} ${dia * arg} `
 			turtle.x -= arg;
 			turtle.y += arg;
 		}
 		else if(inst == "ur") {
-			output += (turtle.inLoop) ? '' : `пр 45 ${mc} ${dia * arg} лв 45 `
+			output += (turtle.inLoop) ? '' : `нк 45 ${mc} ${dia * arg} `
 			turtle.x += arg;
 			turtle.y += arg;
 		}
 		else if(inst == "dl") {
-			output += (turtle.inLoop) ? '' : `пр 225 ${mc} ${dia * arg} лв 225 `
+			output += (turtle.inLoop) ? '' : `нк 225 ${mc} ${dia * arg} `
 			turtle.x -= arg;
 			turtle.y -= arg;
 		}
 		else if(inst == "dr") {
-			output += (turtle.inLoop) ? '' : `пр 135 ${mc} ${dia * arg} лв 135 `
+			output += (turtle.inLoop) ? '' : `нк 135 ${mc} ${dia * arg} `
 			turtle.x += arg;
 			turtle.y -= arg;
 		}
 		else if(["go", "goto", "g"].includes(inst)) {
+			
 			let pNow = turtle
 			let endPos = {x: args[0], y: args[1]}
 			let x = endPos.x - pNow.x + pNow.cx;
 			let y = endPos.y - pNow.y + pNow.cy;
-			let oper = 1
-			
-			if(x < 0)
-				oper = -1
-			let calc = (Math.acos(y / Math.sqrt(y * y + x * x)) / Math.PI * 180).toFixed(5); // calculate rotation and convert to degrees
-			let dist = Math.sqrt(y * y + x * x);
-			output += (turtle.inLoop) ? '' : `${oper > 0 ? "пр" : "лв"} ${calc} вп ${dist} ${oper < 0 ? "пр" : "лв"} ${calc} `.replaceAll('.', ',')
-			turtle.x = args[0]
-			turtle.y = args[1]
+			if(x != 0 || y != 0) {
+				let oper = 1
+				
+				if(x < 0)
+					oper = -1
+				let calc = (Math.acos(y / Math.sqrt(y * y + x * x)) / Math.PI * 180).toFixed(5); // calculate rotation and convert to degrees
+				let dist = Math.sqrt(y * y + x * x);
+				output += (turtle.inLoop) ? '' : `нк 0 ${oper > 0 ? "пр" : "лв"} ${calc} вп ${dist} ${oper < 0 ? "пр" : "лв"} ${calc} `.replaceAll('.', ',')
+				turtle.x = args[0]
+				turtle.y = args[1]
+			}
 		}
 		
 		else if(["rgo", "rgoto", "rg"].includes(inst)) {
-			let pNow = turtle
-			let endPos = {x: args[0] + pNow.x, y: args[1] + pNow.y}
-			let x = endPos.x - pNow.x;
-			let y = endPos.y - pNow.y;
+			let x = args[0]
+			let y = args[1]
 			
 			let oper = 1
 			
 			if(x < 0)
 				oper = -1
-			let calc = (Math.acos(y / Math.sqrt(y * y + x * x)) / Math.PI * 180).toFixed(5); // calculate rotation and convert to degrees
 			let dist = Math.sqrt(y * y + x * x);
-			output += (turtle.inLoop) ? '' : `${oper > 0 ? "пр" : "лв"} ${calc} вп ${dist} ${oper < 0 ? "пр" : "лв"} ${calc} `.replaceAll('.', ',')
-			turtle.x = turtle.x + args[0]
-			turtle.y = turtle.y + args[1]
+			let calc = (Math.acos(y / dist) / Math.PI * 180).toFixed(5); // calculate rotation and convert to degrees
+			output += (turtle.inLoop) ? '' : `нк 0 ${oper > 0 ? "пр" : "лв"} ${calc} вп ${dist} ${oper < 0 ? "пр" : "лв"} ${calc} `.replaceAll('.', ',')
+			turtle.x += args[0]
+			turtle.y += args[1]
 			
 		}
 		else if(inst == "pu")
@@ -127,15 +128,53 @@ function compile() {
 		else if(["sc", "sz"].includes(inst)) {
 			turtle.cx = turtle.x
 			turtle.cy = turtle.y
+		}
+		else if(["sp", "setpoint"].includes(inst)) {
+			turtle.px = turtle.x
+			turtle.py = turtle.y
 			
 		}
+		else if(["ts", "tostart", "end"].includes(inst)) {
+			if(0 != turtle.x || 0 != turtle.y) {
+				let x = 0 - turtle.x;
+				let y = 0 - turtle.y;
+				let oper = 1
+				
+				if(x < 0)
+					oper = -1
+				let dist = Math.sqrt(y * y + x * x);
+				let calc = (Math.acos(y / dist) / Math.PI * 180).toFixed(5); // calculate rotation and convert to degrees
+				output += (turtle.inLoop) ? '' : `нк 0 ${oper > 0 ? "пр" : "лв"} ${calc} вп ${dist} ${oper < 0 ? "пр" : "лв"} ${calc} `.replaceAll('.', ',')
+				turtle.x = 0
+				turtle.y = 0
+			}
+		}
+		else if(["back", "tp"].includes(inst)) {
+			if(turtle.px != turtle.x || turtle.py != turtle.y) {
+				let x = turtle.px - turtle.x;
+				let y = turtle.py - turtle.y;
+				let oper = 1
+				
+				if(x < 0)
+					oper = -1
+				let dist = Math.sqrt(y * y + x * x);
+				let calc = (Math.acos(y / dist) / Math.PI * 180).toFixed(5); // calculate rotation and convert to degrees
+				output += (turtle.inLoop) ? '' : `нк 0 ${oper > 0 ? "пр" : "лв"} ${calc} вп ${dist} ${oper < 0 ? "пр" : "лв"} ${calc} `.replaceAll('.', ',')
+				turtle.x = turtle.px
+				turtle.y = turtle.py
+			}
+		}
+		
 		else {
 			output += Finst
+			output += " "
 		}
 		//debugger
-		console.log(turtle)
+		//console.log(turtle)
 		
 	}
+	//console.log(output)
+	output = output.replaceAll(/ \s*/gi, " ")
 	
     document.getElementById("o").value = output
 }
@@ -195,10 +234,23 @@ function reformPasses() {
 			return
 		elem = document.createElement('textarea');
 		elem.id = i;
-		elem.style = "width:200px;height:200px";
+		elem.style = "width:100px;height:400px";
 		elem.placeholder = `print code of function "${i.slice(5)}" here...`;
 		document.getElementById('passes').appendChild(elem)
 	})
+	
+}
+
+function u() {
+	document.getElementById('i').value = "* 20\nsp\npd\nre 6\ncall u1\ner\npu\nback\nrgoto 0 -60\nнц 108\n\nsp\npd\nre 6\ncall u2\ner\npu\nback\nrgoto 0 -60\nнц 23\n\n\nsp\npd\nre 6\ncall u3\ner\npu\nback\nrgoto 0 -60\nнц 18\n\nsp\npd\nre 6\ncall u4\ner\npu\nback\nrgoto 0 -60\nнц 137\n\n\nsp\npd\nre 6\ncall u5\ner\npu\nback\nrgoto 0 -60\nнц 59\n\n\nsp\npd\nre 6\ncall u6\ner\npu\nback\nrgoto 0 -60\nнц 54\n\n\nsp\npd\nre 6\ncall u7\ner\npu\nback\nend\nнц 129\nнц 38\n\n\n\n"
+	reformPasses();
+	document.getElementById('func_u1').value = 'u 2\nr 1\nd 2\nr 1'
+	document.getElementById('func_u2').value = 'u 1\nr 1\nu 1\nr 2\nd 2\nr 1'
+	document.getElementById('func_u3').value = 'pd\nur 1\nu 1\nr 1\nd 1\ndr 1\npu\nr 1'
+	document.getElementById('func_u4').value = 'u 2\nr 1\ndr 1\ndl 1\nl 1\npu\nr 2\npd'
+	document.getElementById('func_u5').value = 'rgoto 20 40\nrgoto 20 -40\npu\nr 1\npd'
+	document.getElementById('func_u6').value = 'u 2\nr 3\nd 1\nl 2\nd 1\nr 3'
+	document.getElementById('func_u7').value = 'u 2\npu\nr 1\npd\ndl 1\ndr 1\npu\nr 1\npd'
 	
 }
 
